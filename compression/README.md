648dcf3adc96d43b8e75f47e9cb8e3fa436e4d50
seasidegong <SeasideGong@naver.com>
2026-06-27 20:52:11 +0900

    made uncompression

--- compression/README.md
+++ compression/README.md
@@ -6,18 +6,28 @@
 
 ## 2.Working Functions
 - compressionForPrint()
+- uncompression
 
 ### Expected Output
 - expected output
 ```
   input :
   abbcccddddeeeee
-  output :
-  1100 1101 1101 111 111 111 10 10 10 10 0 0 0 0 0
+  
+  output:
+  code: e: 0  d: 10  a: 1100  b: 1101  c: 111
+  compressed line: 1100 1101 1101 111 111 111 10 10 10 10 0 0 0 0 0
+  uncompressed line: abbcccddddeeeee
 ```
 
 ### How it works
+#### Compress :
 - Counts the frequency of each char.
-- Sort them by frequency in descending order..
+- Sort them by frequency in descending order.
 - Assigns shorter bit if it has higher frequency.
-- Change string line to bit.
+- Change vector of chars to bit.
+
+#### Uncompress :
+- Find compressed data in the code and get the matching value
+- Put value in vector
+- Change vector to string
--- compression/main.cpp
+++ compression/main.cpp
@@ -2,6 +2,7 @@
 #include <format>
 #include <map>
 #include <ranges>
+#include <stdexcept>
 #include <string>
 #include <vector>
 #include <memory>
@@ -10,14 +11,14 @@
 #include <algorithm>
 #include <sstream>
 
-class Line {
+class HuffmanCode {
 public:
-
-    std::string line_ = "";
-
-    void write(std::string line) {
-        line_ = line;
-    }
+    using DataType = char;
+    using Data = std::vector<DataType>;
+    using Compressed = std::vector<bool>;
+    using CompressionCode = std::map<DataType, Compressed>;
+    using Frequency = std::map<DataType, int>;  // <DataType, count>
+    using UncompressionCode = std::map<Compressed, DataType>;
 
     // There are no spaces between words.
     // std::vector<bool> compression() {
@@ -68,23 +69,24 @@ public:
     //     return codedLine;
     // }
 
-    std::vector<std::vector<bool>> compressionForPrint() {
-        std::vector<char> chars(line_.begin(), line_.end());
-        std::map<char, int> frequency;
+    std::pair<UncompressionCode, std::vector<Compressed>> compressionForPrint(const std::string& line) const {
+        Data chars(line.begin(), line.end());
+        Frequency frequency;
         for (const auto& c : chars) {
             frequency[c]++;
         }
 
+        // to sort frequency, used vector to contain char and int.
         std::vector<std::pair<char, int>> vecFrequency(frequency.begin(), frequency.end());
 
         std::sort(vecFrequency.begin(), vecFrequency.end(), [](const auto& a, const auto& b) {
             return a.second > b.second;
         });
 
-        std::map<char, std::vector<bool>> code;
+        CompressionCode code;
 
         int index = 0;
-        std::vector<bool> boolStacked;
+        Compressed boolStacked;
         for (const auto& v : vecFrequency) {
             std::vector<bool> boolLine(boolStacked);
 
@@ -105,18 +107,23 @@ public:
             index++;
         }
 
-        std::vector<std::vector<bool>> codedLine;
+        std::vector<Compressed> compressedLine;
         for (const auto& c : chars) {
             const auto it = code.find(c);
             if (it != code.end()) {
-                codedLine.emplace_back(it->second);
+                compressedLine.emplace_back(it->second);
             }
         }
 
-        return codedLine;
+        UncompressionCode codeForUncompression;
+        for (const auto& c : code) {
+            codeForUncompression.emplace(c.second, c.first);
+        }
+
+        return std::make_pair(codeForUncompression, compressedLine);
     }
 
-    int getSum(const std::vector<std::pair<char, int>>& vecFrequency, int start) {
+    int getSum(const std::vector<std::pair<char, int>>& vecFrequency, int start) const {
         int index = 0;
         int result = 0;
         for (const auto& n : vecFrequency) {
@@ -128,13 +135,29 @@ public:
         return result;
     }
 
-};
+    std::string uncompression(std::pair<UncompressionCode, std::vector<Compressed>> codeAndCompressedData) const{
+        const auto& code = codeAndCompressedData.first;
+        const auto& compressedData = codeAndCompressedData.second;
+        Data data;
+        for (const auto& v : compressedData) {
+            auto it = code.find(v);
+            if (it != code.end()) {
+                data.emplace_back(it->second);
+            } else {
+                // if compressed data can not be found in the code, it will be replaced with '!'".
+                data.emplace_back('!');
+            }
+        }
 
+        std::string stringData(data.begin(), data.end());
+        return stringData;
+    }
+};
 
 int main() {
 
-    Line line;
-    line.write("abbcccddddeeeee");
+    HuffmanCode huffmanCode;
+
     // for (const auto& code : line.compression())  {
     //     if (code) {
     //         std::cout << "1";
@@ -143,12 +166,31 @@ int main() {
     //     }
     // }
     // std::cout << std::endl;
+    std::string inputLine = "abbcccddddeeeee";
     std::cout << "input :" << std::endl;
-    std::cout << line.line_ << std::endl;
+    std::cout << inputLine << std::endl << std::endl;
 
-    std::cout << "output :" << std::endl;
-    for (const auto& vec : line.compressionForPrint())  {
-        for (const auto& code : vec) {
+    std::cout << "output:" << std::endl;
+    std::cout << "code: ";
+    const auto& codeAndCompressedData = huffmanCode.compressionForPrint(inputLine);
+
+    for (const auto& vec : codeAndCompressedData.first)  {
+        std::cout << vec.second << ": ";
+        for (const auto& code : vec.first) {
+            if (code) {
+                std::cout << "1";
+            } else {
+                std::cout << "0";
+            }
+        }
+        std::cout << "  ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "compressed line: ";
+
+    for (const auto& compressed : codeAndCompressedData.second)  {
+        for (const auto& code : compressed) {
             if (code) {
                 std::cout << "1";
             } else {
@@ -159,11 +201,18 @@ int main() {
     }
     std::cout << std::endl;
 
+    std::cout << "uncompressed line: ";
+    std::cout << huffmanCode.uncompression(codeAndCompressedData)<< std::endl;
+
+
     // expected output
     // input :
     // abbcccddddeeeee
-    // output :
-    // 1100 1101 1101 111 111 111 10 10 10 10 0 0 0 0 0
+
+    // output:
+    // code: e: 0  d: 10  a: 1100  b: 1101  c: 111
+    // compressed line: 1100 1101 1101 111 111 111 10 10 10 10 0 0 0 0 0
+    // uncompressed line: abbcccddddeeeee
 
     return 0;
 }
